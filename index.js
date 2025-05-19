21623ac2391293f21cc85a009210771efd8b6a98
ju4ncb <ju4ncb@gmail.com>
2025-05-18 21:19:38 -0500

    server config

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import express from "express";
-import colors from "colors";
+import express from 'express';
+// import colors from 'colors';
+import config from './server.js';
 
 const server = express();
 
 server.listen(3000, () => {
   console.log(
-    "Servidor backend de themis encendido puesto en el puerto 3000,".green,
-    "http://localhost:3000".cyan
+    'Servidor backend de themis encendido puesto en el puerto 3000,'.green,
+    `http://localhost:${config.port}`.cyan,
   );
 });
 
-server.get("/", (req, res) => {
-  res.send("<h1>Prueba</h1>");
+server.get('/', (req, res) => {
+  res.send('<h1>Prueba</h1>');
   res.end();
 });
